7e0297900662d4bccf78cacf3f0520af27fcdfda
AnubrataDS <anubrata.stephenite.nrg@gmail.com>
2025-03-30 19:59:48 +0530

    bgm

--- main-menu-scene/main-menu-background-scene/spinner.gd
+++ main-menu-scene/main-menu-background-scene/spinner.gd
@@ -1,6 +1,6 @@
 extends CSGMesh3D
-
+var speed = Vector3(randf_range(-5,5),randf_range(-5,5),randf_range(-5,5))
 func _physics_process(delta: float) -> void:
-	rotate_x(delta)
-	rotate_y(-delta)
-	rotate_z(delta*2)
+	rotate_x(delta*speed.x)
+	rotate_y(delta*speed.y)
+	rotate_z(delta*speed.z)
